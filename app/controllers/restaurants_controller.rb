5056fa349482bc17d49153069e9a61784c62a68a
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-20 11:14:09 +0100

    added flash notice and link

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -29,7 +29,10 @@ class RestaurantsController < ApplicationController
   end
 
   def destroy
-
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
   end
 
 
